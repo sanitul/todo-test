d4882ca0c2f8318474c2d7ef067b1fc33733660d
Sanid <sanitul@bs-23.com>
2022-06-27 15:44:51 +0600

    code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import AddTodo from './features/Todo/components/AddTodo';
 import TodoList from './features/Todo/components/TodoList';
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [count, setCount] = useState<number>(0);
 
 	return (
 		<div className="app">
